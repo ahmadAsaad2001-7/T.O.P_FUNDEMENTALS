a5f288d492a7811b374d72f927a17d90f8d15c9d
ahmad <ahmedasaad71421@gmail.com>
2024-12-22 18:06:13 +0200

    beforechanges

--- rockoaperscissors/proj.js
+++ rockoaperscissors/proj.js
@@ -1,54 +1,31 @@
+// the JavaScript file
+// METHODS 2 & 3
+function alertFunction() {
+  alert("YAY! YOU DID IT!");
+}
+const btn = document.querySelector("#btn");
 
-function playRound(humanChoice, computerChoice) {
-    let hscore=0,cscore=0;
-    let numberOfRounds=parseInt(prompt("enter the number of rounds"));
-    // your code here!
-    for(let i=0 ;i<numberOfRounds;i++){
-        let humanChoice = getHumanChoice(); 
-        let computerChoice = getComputerChoice();
-    if (humanChoice==computerChoice)
-    {
-        console.log("draw");
-    }
-    if(humanChoice=="rock" && computerChoice=="scissors")
-    {console.log("user wins");
-        hscore++;
-    }
-    if(humanChoice=="scissors" && computerChoice=="paper")
-    {console.log("user wins");
-        hscore++;
-    }
-    if(humanChoice=="paper" && computerChoice=="rock")
-    {console.log("user wins");
-        hscore++;
-    }
-   
-    if(computerChoice=="rock" &&  humanChoice=="scissors")
-     {console.log("computer wins");
-        cscore++;
-     }
-    if(computerChoice=="scissors" && humanChoice=="paper")
-     {console.log("computer wins");
-        cscore++;
-     }
-    if(computerChoice=="paper" && humanChoice=="rock")
-     {console.log("computer wins");
-        cscore++;
-     }
-    }
+// METHOD 2
+btn.onclick = alertFunction;
 
-      console.log(`Final score: User ${hscore}, Computer ${cscore}`);
-    }
-  function getHumanChoice(){
-let hum=prompt("pick one from the following rock - paper scissors");
-return hum.toLowerCase();
-  }
-  function getComputerChoice(){
-    let comp=["rock","paper","scissors"];
-    return comp[Math.floor(Math.random()*comp.length)];
-  }
-  
-  
-  
-  playRound();
-  
+// METHOD 3
+btn.addEventListener("click", alertFunction);
+// your JavaScript file
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+
+
+const parag = document.createElement("p");
+content.classList.add("parag");
+content.textContent = "This is the glorious text-parag!";
+
+
+
+
+container.appendChild(content);
+
+container.appendChild(parag);
+
